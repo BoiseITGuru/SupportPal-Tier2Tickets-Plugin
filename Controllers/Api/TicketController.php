672f5fd980b7ab53d2139c5bc4c4647d791b5024
Technlogy Advocates <info@tech-advocates.com>
2022-02-13 09:34:58 +0000

    worked on ticket create endpoint

--- Controllers/Api/TicketController.php
+++ Controllers/Api/TicketController.php
@@ -5,8 +5,11 @@ namespace App\Plugins\HelpdeskButtonsAPIEmulator\Controllers\Api;
 use App\Modules\Core\Controllers\BaseApiController;
 use App\Modules\Core\Models\ActivityLog;
 use App\Modules\Ticket\Models\Ticket;
+use App\Modules\User\Models\User;
+use App\Modules\User\Models\UserOrganisationDomain;
 use App\Plugins\HelpdeskButtonsAPIEmulator\Requests\Api\Tickets\StoreRequest;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use GuzzleHttp\Client;
 
 use function call_user_func;
 use function count;
@@ -16,7 +19,7 @@ use function trans;
 class TicketController extends BaseApiController
 {
     /**
-     * TimeLogController constructor.
+     * TicketController constructor.
      */
     public function __construct()
     {
@@ -29,29 +32,72 @@ class TicketController extends BaseApiController
      */
     public function createTicket(StoreRequest $request)
     {
-        // /** @var Ticket $ticket */
-        // $ticket = Ticket::findOrFail($request->input('ticket_id'));
+        /**
+         * TO DO
+         * Send Ticket Created Email
+         * Look up department ticket number format
+         * Look up department email id
+         * Save description as message
+         * Map Priorites
+         * Map Status
+         * Support Email Aliases (Need to create plugin first)
+         */
 
-        // $inputs = ['ticket_id', 'user_id', 'time', 'description', 'billable'];
 
-        // $record = new TimeLog;
-        // $record->fill($request->all($inputs));
-        // $record->status = 2;
-        // $record->created_at = $request->input('start_time');
-        // $record->save();
 
-        Ticket::create([
-            'user_email'   => $request->input('email'),
-            'department_id'   => 1,
-            'brand_id'   => 1,
-            'status'       => $request->input('status'),
-            'priority'     => $request->input('priority'),
-            'subject'      => $request->input('subject'),
-            'text'         => $request->input('description')
+        //Get POST Data
+        $email = $request->input('email');
+        $subject = $request->input('subject');
+        $status = $request->input('status');
+        $priority = $request->input('priority');
+        $description = $request->input('description');
+        $brand_id = $request->input('custom_fields.brand');
+        $department_id = $request->input('custom_fields.department');
+        $source = $request->input('source');
+
+        //Check if user exists
+        $whereCondition = [
+            ['email', '=', $email],
+            ['brand_id', '=', $brand_id]
+        ];
+        $user = User::where($whereCondition)->first();
+
+        if (!$user) {
+            //Get org by domain if exists
+            $domain = substr($email, strpos($email, '@') + 1);
+            $org = UserOrganisationDomain::where('domain', $domain)->first();
+
+            //Create User
+            if ($org) {
+                $user = User::create([
+                    'brand_id'          => $brand_id,
+                    'email'             => $email,
+                    'organisation_id'   => $org->organisation_id
+                ]);
+            } else {
+                $user = User::create([
+                    'brand_id'          => $brand_id,
+                    'email'             => $email
+                ]);
+            }
+        }
+
+
+        //Create Ticket
+        $digits = 7;
+        $ticket = Ticket::create([
+            'number'            => rand(pow(10, $digits-1), pow(10, $digits)-1),
+            'channel_id'        => 3,
+            'user_id'           => $user->id,
+            'department_id'     => $department_id,
+            'brand_id'          => $brand_id,
+            'status_id'         => $status,
+            'priority_id'       => $priority,
+            'subject'           => $subject,
+            'text'              => $description
         ]);
 
-        // // Update time logged on ticket
-        // $this->updateTicketTime($ticket->id);
+        return $ticket;
 
         // return $this->response->array([
         //     'status'  => 'success',
@@ -60,7 +106,5 @@ class TicketController extends BaseApiController
         //     ]),
         //     'data'    => $record->loadMissing(['ticket', 'user'])
         // ]);
-
-        return ;
     }
 }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ce4121b2ad8b0bc42f41ac7727452330b0995c03',
+        'name' => 'technologyadvocates/helpdeskbuttonsapiemulator',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'technologyadvocates/helpdeskbuttonsapiemulator' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ce4121b2ad8b0bc42f41ac7727452330b0995c03',
+            'dev_requirement' => false,
+        ),
+    ),
+);
